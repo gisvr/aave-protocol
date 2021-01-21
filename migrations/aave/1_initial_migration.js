e36c04a9c15a4201cc2e988bf159bd32688f9d31
liyu <txdev6@gmail.com>
2021-01-21 10:54:37 +0800

    add: geth node

--- migrations/aave/1_initial_migration.js
+++ migrations/aave/1_initial_migration.js
@@ -1,7 +1,9 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer,network,accounts) {
-    console.log(network);
-    console.log(accounts);
+module.exports = function (deployer,network,accounts) { 
+    let sender = accounts[0]
+    let senderBal = await web3.eth.getBalance(sender);
+    // let chainId = await web3.eth.getChainId();
+    console.log(`Network %s Sender %s Bal %s`,network,sender,senderBal.toString()); //
     deployer.deploy(Migrations);
 };
